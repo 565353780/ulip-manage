da9ac7bfb038fd351cf0381adfc64692ba70a455
chLi <565353780@qq.com>
2024-11-11 00:07:32 +0800

    simplify args load

--- ulip_manage/Demo/detector.py
+++ ulip_manage/Demo/detector.py
@@ -1,13 +1,11 @@
-from ulip_manage.Config.config import getConfig
 from ulip_manage.Module.detector import Detector
 
 def demo():
-    args = getConfig()
     model_file_path = '/home/chli/chLi/Model/ULIP2/pretrained_models_ckpt_zero-sho_classification_pointbert_ULIP-2.pt'
     open_clip_model_file_path = '/home/chli/Model/CLIP-ViT-bigG-14-laion2B-39B-b160k/open_clip_pytorch_model.bin'
     device = 'cuda:0'
 
-    detector = Detector(args, model_file_path, open_clip_model_file_path, device)
+    detector = Detector(model_file_path, open_clip_model_file_path, device)
 
     pcd_file_path = '/home/chli/chLi/Dataset/SampledPcd_Manifold/ShapeNet/03001627/46bd3baefe788d166c05d60b45815.npy'
     points_feature = detector.encodePointCloudFile(pcd_file_path)
